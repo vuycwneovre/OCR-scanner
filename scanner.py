272cc5ab13b9eb14a3ec32e6c065a84d692f6b43
vuycwneovre <79833851+vuycwneovre@users.noreply.github.com>
2021-09-14 14:27:43 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,18 +1,13 @@
-from typing import Text
 import pytesseract
 from PIL import Image 
 
 
-pytesseract.pytesseract.tesseract_cmd = r'<C:\Users\admin\AppData\Local\Programs\Tesseract-OCR\tesseract.exe>'
+pytesseract.pytesseract.tesseract_cmd = '<C:/Users/admin/AppData/Local/Programs/Tesseract-OCR/tesseract.exe>'
 
 img = Image.open('C:/Users/admin/Documents/github-upload/camera_scanner/test2.png')
 
 def ocr_core(img):
     text = pytesseract.image_to_string(img)
-    print(text)
     return text
 
 print(ocr_core(img))
-
-def main():
-    print(ocr_core())
